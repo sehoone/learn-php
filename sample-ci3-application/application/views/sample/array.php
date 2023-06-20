d764ea87c91a79c1f29d3e653cc77149f484b391
SeHoon <sehoon1989@gmail.com>
2023-06-21 00:25:40 +0900

    add-array-example

--- sample-ci3-application/application/views/sample/array.php
+++ sample-ci3-application/application/views/sample/array.php
@@ -11,10 +11,18 @@
 
 	// array 정의 (key-value)
 	$people3 = ['sehoone', 11 => "iu"];
+
+	// array 정의(문자열을 사용한 key-value)
+	$people4 = [
+		'twitter' => 'pratham',
+		'web3' => 'vitto',
+		'feedhive' => 'simon'
+	];
 ?>
 <p>people1 <?php echo $people1[0] ?>입니다.</p>
 <p>people2 <?php echo $people1[1] ?>입니다.</p>
 <p>people3 <?php echo $people3[11] ?>입니다.</p>
+<p>people3 <?php echo $people4['twitter'] ?>입니다.</p>
 
 </body>
 </html>
